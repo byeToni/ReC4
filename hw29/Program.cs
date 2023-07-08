23eff614c24e490ac38cffc46032ada2c41d7327
A_SH <hciveegress@mail.ru>
2023-07-08 13:31:51 +0300

    hw29

---
+++ hw29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] coll)
+{
+    
+    for (int i = 0; i < coll.Length; i++)
+    {
+
+        coll[i] = new Random().Next(1, 100);
+    }
+
+}
+
+void OutptArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} : ");
+    }
+}
+
+int[] arr = new int[8];
+Console.Clear();
+FillArray(arr);
+OutptArray(arr);
