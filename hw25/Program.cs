28c4b72caccc1bb70322f5e524b13e2671df2987
A_SH <hciveegress@mail.ru>
2023-07-08 13:26:19 +0300

    hw25

---
+++ hw25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int rez = Convert.ToInt32(Console.ReadLine());
+    return rez;
+}
+
+Console.Clear();
+Console.WriteLine("Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.  ");
+int rezult=1;
+int a = GetNumber("Введите число А: ");
+int b = GetNumber("Введите число B: ");
+for (int i = 0; i < b; i++)
+{
+    rezult *= a;
+}
+Console.WriteLine($"Результат {rezult}");
